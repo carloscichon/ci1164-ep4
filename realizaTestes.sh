6b6b2b65b71f3e79e90bea61e9c06fdfd1e9e997
Carlos Eduardo Cichon Henriques <cech19@inf.ufpr.br>
2021-07-09 21:24:43 -0300

    Adiciona FLOP_DP
    Signed-off-by: Carlos Eduardo Cichon Henriques <cech19@inf.ufpr.br>

--- realizaTestes.sh
+++ realizaTestes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-TAMANHOS="64 100"
-TESTES="L3 L2CACHE FLOPS_DP"
+#TAMANHOS="64 100 128 2000 2048"
+TAMANHOS="64"
+
+TESTES="FLOPS_DP"
 CORE_ID=3
 
 for teste in $TESTES; do
@@ -11,18 +13,24 @@ for teste in $TESTES; do
             cat tmp.txt | grep "L3 bandwidth" | cut -d'|' -f3 >> "$teste"_"$tam".txt
         elif [ "$teste" = "L2CACHE" ]; then
             cat tmp.txt | grep "miss ratio" | cut -d'|' -f3 >> "$teste"_"$tam".txt
-        # FLOPS_DP ainda não funciona
         elif [ "$teste" = "FLOPS_DP" ]; then
-            DP=$(cat tmp.txt | grep "DP " | cut -d'|' -f3) 
-            echo $DP
-            AVX=$(cat tmp.txt | grep "AVX " | cut -d'|' -f3) 
-            echo "$DP | $AVX" >> "$teste"_"$tam".txt
-            
+            cat tmp.txt | grep "DP " | cut -d'|' -f3 >> "$teste"_"$tam"_dp.txt            
+            cat tmp.txt | grep "AVX " | cut -d'|' -f3 >> "$teste"_"$tam"_avx.txt
+            # retira linha por linha dos arquivos dp e avx
+            for i in $TAMANHOS; do
+                DP=$(head -1 "$teste"_"$tam"_dp.txt)
+                AVX=$(head -1 "$teste"_"$tam"_avx.txt)
+                echo "$DP | $AVX" >> "$teste"_"$tam".txt
+                sed -i '1d' "$teste"_"$tam"_dp.txt
+                sed -i '1d' "$teste"_"$tam"_avx.txt
+            done
+            #rm "$teste"_"$tam"_dp.txt "$teste"_"$tam"_avx.txt
         fi
         while read line; do
                 echo "$tam | $line" >> $teste.txt    
             done < "$teste"_"$tam".txt
         
         rm "$teste"_"$tam".txt
+        rm tmp.txt
     done
 done
